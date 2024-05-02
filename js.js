17b8e0d439c3efc66bb39cb24d708a08073212b2
Thiago Masseno <thiagomasseno3@gmail.com>
2024-05-02 17:53:54 -0300

    first commit

---
+++ js.js
@@ -0,0 +1,71 @@
+let niveis = []
+
+const objeto1 = {
+    matriz:[
+        [1,2,3,4],
+        [4,3,2,1],
+        [1,2,3,4],
+        [4,3,2,1]
+    ]
+    ,
+    coluna : 31
+}
+
+const objeto2 = {
+    matriz:[
+        [6,7,8,9],
+        [9,8,7,6],
+        [6,7,8,9],
+        [9,8,7,6]
+    ]
+    ,
+    coluna : 32
+}
+
+niveis[0].push(objeto1)
+
+console.log(niveis[0])
+
+const objeto3 = {
+    matriz:[
+        [5,6,7,8],
+        [8,7,6,5],
+        [5,6,7,8],
+        [8,7,6,5]
+    ]
+    ,
+    coluna : 23
+}
+
+const objeto4 = {
+    matriz:[
+        [5,6,7,8],
+        [8,7,6,5],
+        [5,6,7,8],
+        [8,7,6,5]
+    ]
+    ,
+    coluna : 15
+}
+
+const objeto5 = {
+    matriz:[
+        [3,4,5,6],
+        [6,5,4,3],
+        [3,4,5,6],
+        [6,5,4,3]
+    ]
+    ,
+    coluna : 17
+}
+
+const objeto6 = {
+    matriz:[
+        [7,6,5,4],
+        [4,5,6,7],
+        [7,6,5,4],
+        [4,5,6,7]
+    ]
+    ,
+    coluna : 18
+}
